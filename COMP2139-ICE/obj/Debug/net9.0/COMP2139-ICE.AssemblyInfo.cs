6a16a306577c8632411a5c7fdff2b7ba03ee9473
Sofia Janik <janiksofia1@gmail.com>
2025-09-20 20:50:16 -0400

    Lab 2 completed

--- COMP2139-ICE/obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
+++ COMP2139-ICE/obj/Debug/net9.0/COMP2139-ICE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f47bd068b67451fa286b67484bd1c108cd27e36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("COMP2139-ICE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
